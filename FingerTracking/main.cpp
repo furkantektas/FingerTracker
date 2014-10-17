98c3702f90f67bd732a433e3856cfa1f41d97003
Furkan Tektas <tektasfurkan@gmail.com>
2014-10-17 18:56:56 +0300

    Initial Commit

---
+++ FingerTracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FingerTracking
+//
+//  Created by Furkan Tektas on 10/17/14.
+//  Copyright (c) 2014 Furkan Tektas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
