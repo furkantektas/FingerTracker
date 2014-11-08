9e757b701acfcc37053cfb444836bd5831c98317
Furkan Tektas <tektasfurkan@gmail.com>
2014-11-08 17:59:07 +0200

    Adding color header

---
+++ FingerTracking/colors.h
@@ -0,0 +1,25 @@
+//
+//  colors.h
+//  FingerTracking
+//
+//  Created by Furkan Tektas on 11/8/14.
+//  Copyright (c) 2014 Furkan Tektas. All rights reserved.
+//
+
+#ifndef FingerTracking_colors_h
+#define FingerTracking_colors_h
+
+#define RED Scalar(0,0,255)
+#define GREEN Scalar(0,255,0)
+#define BLUE Scalar(255,0,0)
+#define LIGHTGRAY Scalar(180,180,180)
+#define GRAY Scalar(128,128,128)
+#define DARKGRAY Scalar(80,80,80)
+#define WHITE Scalar(255,255,255)
+#define BLACK Scalar(0,0,0)
+#define YELLOW Scalar(0,255,255)
+#define PURPLE Scalar(255,0,255)
+#define CYAN Scalar(255,255,0)
+
+
+#endif
